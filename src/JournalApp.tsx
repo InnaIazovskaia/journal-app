eeef33462e18e65d59fbbdb49bb466b2a5a13f28
Inna Iazovskaia <inna.0961@mail.ru>
2023-04-06 11:40:00 +0200

    Implement AppTheme in JournalApp

--- src/JournalApp.tsx
+++ src/JournalApp.tsx
@@ -1,9 +1,10 @@
 import AppRouter from "./router/AppRouter";
+import { AppTheme } from "./theme";
 
 const JournalApp = (): JSX.Element => (
-  <>
+  <AppTheme>
     <AppRouter />
-  </>
+  </AppTheme>
 );
 
 export default JournalApp;
