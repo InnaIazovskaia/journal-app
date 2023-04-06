1d66c4e326c27dc70857612b074933ad9ce30b9a
Inna Iazovskaia <inna.0961@mail.ru>
2023-04-06 23:59:30 +0200

    Add html structure in RegisterPage

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,1 +1,52 @@
-export const RegisterPage = (): JSX.Element => <h1>RegisterPage</h1>;
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+
+export const RegisterPage = (): JSX.Element => (
+  <AuthLayout title="Register">
+    <form>
+      <Grid container>
+        <Grid item xs={12} sx={{ mt: 2 }}>
+          <TextField
+            label="Username"
+            type="text"
+            placeholder="Username"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sx={{ mt: 2 }}>
+          <TextField
+            label="Email"
+            type="email"
+            placeholder="email@googl.com"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sx={{ mt: 2 }}>
+          <TextField
+            label="Password"
+            type="password"
+            placeholder="Password"
+            fullWidth
+          />
+        </Grid>
+
+        <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+          <Grid item xs={12}>
+            <Button variant="contained" fullWidth>
+              Register
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Grid container direction="row" justifyContent="end">
+          <Typography sx={{ mr: 1 }}> Already a member? </Typography>
+          <Link component={RouterLink} color="#f91f6d" to="/auth/login">
+            Log in
+          </Link>
+        </Grid>
+      </Grid>
+    </form>
+  </AuthLayout>
+);
