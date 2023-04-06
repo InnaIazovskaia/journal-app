1518147cae4bdfd95d9fb56b4d3843091573b33e
Inna Iazovskaia <inna.0961@mail.ru>
2023-04-06 19:32:43 +0200

    Create test for AuthLayout

---
+++ src/auth/layout/AuthLayout.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { AuthLayout } from "./AuthLayout";
+
+describe("Given a AuthLayout component", () => {
+  describe("When it's rendered with title text 'Login'", () => {
+    test("Then it should show heading 'Login'", () => {
+      const titleText = "Login";
+
+      render(<AuthLayout title={titleText}>{}</AuthLayout>);
+
+      const title = screen.getByRole("heading", { level: 5, name: titleText });
+
+      expect(title).toBeInTheDocument();
+    });
+  });
+});
