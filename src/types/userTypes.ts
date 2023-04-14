c7c20f43d3d31c8482bc4149bbd17d07ed60f282
Inna Iazovskaia <inna.0961@mail.ru>
2023-04-14 22:53:02 +0200

    Create interface UserData

--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -1,10 +1,13 @@
-export interface UserState {
-  status: "checking" | "not-authenticated" | "authenticated";
+export interface UserData {
   uid: string;
   email: string;
   displayName: string;
   photoURL: string;
   errorMesssage: string;
+}
+
+export interface UserState extends UserData {
+  status: "checking" | "not-authenticated" | "authenticated";
 }
 
 export interface UserCredentials {
