36d4455cd0e7ca4d8e8abce02080d4fab19e9079
Inna Iazovskaia <inna.0961@mail.ru>
2023-04-14 22:49:52 +0200

    Implement signInWIthGoogle in thunks

--- src/store/auth/thunks.ts
+++ src/store/auth/thunks.ts
@@ -1,5 +1,10 @@
+import { signInWIthGoogle } from "../../firebase/providers";
 import { AppDispatch } from "../store";
-import { checkingCredentialsActionCreator } from "./authSlice";
+import {
+  loginUserActionCreator,
+  checkingCredentialsActionCreator,
+  logoutUserActionCreator,
+} from "./authSlice";
 
 export const checkingAuthantication =
   (email: string, password: string) => async (dispatch: AppDispatch) => {
@@ -8,4 +13,20 @@ export const checkingAuthantication =
 
 export const startGoogleSignIn = () => async (dispatch: AppDispatch) => {
   dispatch(checkingCredentialsActionCreator());
+
+  const result = await signInWIthGoogle();
+
+  if (!result.ok) {
+    dispatch(logoutUserActionCreator(result.errorMessage!));
+  }
+
+  dispatch(
+    loginUserActionCreator({
+      uid: result.uid!,
+      displayName: result.displayName!,
+      email: result.email!,
+      photoURL: result.photoURL!,
+      errorMesssage: result.errorMessage!,
+    })
+  );
 };
